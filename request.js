0285543f384c35484f26e0b15e55c78b1c9516de
unknown <aqbautista@up.edu.ph>
2023-04-13 01:10:29 +0800

    test: add test case for delete

--- request.js
+++ request.js
@@ -1,25 +1,31 @@
 import needle from "needle";
 
-const data = {
-    age:44
-}
-const addData = {
-    stdnum: '0987654',
-    fname: 'JM',
-    lname: "Cruz",
-    age: 25
-}
+// const data = {
+//     age:44
+// }
+// const addData = {
+//     stdnum: '0987654',
+//     fname: 'JM',
+//     lname: "Cruz",
+//     age: 25
+// }
 
-needle.post("http://localhost:3000/add-students",{
-    stdnum: addData.stdnum,
-    fname: addData.fname,
-    lname: addData.lname,
-    age: addData.age
-}, (req,res) => {
-    console.log(res.body);
+// needle.post("http://localhost:3000/add-students",{
+//     stdnum: addData.stdnum,
+//     fname: addData.fname,
+//     lname: addData.lname,
+//     age: addData.age
+// }, (req,res) => {
+//     console.log(res.body);
+// })
 
-})
-needle.post("http://localhost:3000/find-students-by-post",
-    {age: data.age}, (req,res)=>{
+// needle.post("http://localhost:3000/find-students-by-post",
+//     {age: data.age}, (req,res)=>{
+//         console.log(res.body);
+//     });
+
+needle.post("http://localhost:3000/delete-student-by-name", 
+    {fname: "Ellie", lname: "Lang"} , (req, res) => {
         console.log(res.body);
-    });
+    }
+);
